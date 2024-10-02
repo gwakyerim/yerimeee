7d088596833744016466a8ff92f68f7c6d0f4eb0
gawkyerim <jh19930@naver.com>
2024-10-02 14:01:14 +0900

    last final portfolio

--- js/custom.js
+++ js/custom.js
@@ -58,8 +58,29 @@ $(function(){
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
-      });
+  });
+  
+  var btn = document.querySelectorAll("button.modalOpenButton");
+  var modals = document.querySelectorAll('.modalContainer');
+  var spans = document.getElementsByClassName("modalCloseButton");
+
+  for (var i = 0; i < btn.length; i++) {
+    btn[i].onclick = function (e) {
+      e.preventDefault();
+      modal = document.querySelector(e.target.getAttribute("href"));
+      modal.style.display = "block";
+    }
       
+    spans[i].onclick = function () {
+      for (var index in modals) {
+        if (typeof modals[index].style !== 'undefined');
+          modals[index].style.display = "none";
+      }
+    }
+  }
+  $('.modalContainer').click(function(){
+    $(this).hide();
+  })
   /* Mobile Custom JS */
   $('.hamburger').click(function(){
     $('.main-menu').toggleClass('active');
